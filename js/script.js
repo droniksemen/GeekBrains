2c85410153558e5121f6dc29312f52e6220cd2fd
droniksemen <AndreySemen@ya.ru>
2018-11-24 13:45:50 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function init(){
+	var images = document.getElementsByTagName("img");
+
+	for (var i = 0; i < images.length; i++) {
+		//images[i].onclick = changeBigPicture;
+		images[i].addEventListener("click", changeBigPicture);
+	}
+}
+
+function changeBigPicture(event){
+	var appDiv = document.getElementById("big_picture");
+	appDiv.innerHTML = "";	
+	var eventElement = event.target;
+	var imageNumber = eventElement.getAttribute("data-image");
+	var src = "img/big/" + imageNumber + ".jpg";
+	var imageDomElement = document.createElement("img");
+	imageDomElement.src = src;
+
+	imageDomElement.setAttribute('onerror', 'alert("Нет такой картинки")' );
+
+	appDiv.appendChild(imageDomElement);
+}
+
+window.onload = init;
